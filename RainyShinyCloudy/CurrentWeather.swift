fbd32f80fecd544d351e8bab52fda6540089f8a0
mshayanh13 <mohammadhemani@gmail.com>
2017-02-09 15:58:47 -0600

    Parse JSON Data

--- RainyShinyCloudy/CurrentWeather.swift
+++ RainyShinyCloudy/CurrentWeather.swift
@@ -50,13 +50,40 @@ class CurrentWeather {
         return _currentTemp
     }
     
-    func downloadWeatherDetails(completed: DownloadComplete) {
+    func downloadWeatherDetails(completed: @escaping DownloadComplete) {
         //Alamofire download
         let currentWeatherURL = URL(string: CURRENT_WEATHER_URL)!
         Alamofire.request(currentWeatherURL).responseJSON { response  in
             let result = response.result
-            print(result)
+            
+            if let dict = result.value as? Dictionary<String, Any> {
+                
+                if let name = dict["name"] as? String {
+                    self._cityName = name.capitalized
+                    print(self._cityName)
+                }
+                
+                if let weather = dict["weather"] as? [Dictionary<String,Any>] {
+                    
+                    if let main = weather[0]["main"] as? String {
+                        self._weatherType = main.capitalized
+                        print(self._weatherType)
+                    }
+                }
+                
+                if let main = dict["main"] as? Dictionary<String,Any> {
+                    
+                    if let currentTemperature = main["temp"] as? Double {
+                        
+                        let kelvinToFarenheitPreDivision = (currentTemperature * 9/5 - 459.67)
+                        let kelvinToFarenheit = Double(round(10*kelvinToFarenheitPreDivision/10))
+                        
+                        self._currentTemp = kelvinToFarenheit
+                        print(self._currentTemp)
+                    }
+                }
+            }
+            completed()
         }
-        completed()
     }
 }
