9c7f2e80ab9886940a628fa40384b352cbf2e210
mshayanh13 <mohammadhemani@gmail.com>
2017-02-09 15:45:59 -0600

    Downloading data from the API with Alamofire

--- RainyShinyCloudy/Constants.swift
+++ RainyShinyCloudy/Constants.swift
@@ -14,4 +14,6 @@ let LONGITUDE = "&lon="
 let APP_ID = "&appid="
 let APP_KEY = "89575d3c850c4fe09a01e9aedf6aec9e"
 
+typealias DownloadComplete = () -> ()
+
 let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(APP_KEY)"
