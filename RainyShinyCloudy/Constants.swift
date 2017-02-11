7ca902f7755e68a3d6c369d93fcff90910d8ca56
mshayanh13 <mohammadhemani@gmail.com>
2017-02-11 15:02:29 -0600

    Data modeling and initializing weather Dictionary

--- RainyShinyCloudy/Constants.swift
+++ RainyShinyCloudy/Constants.swift
@@ -17,3 +17,4 @@ let APP_KEY = "89575d3c850c4fe09a01e9aedf6aec9e"
 typealias DownloadComplete = () -> ()
 
 let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(APP_KEY)"
+let FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=-36&lon=123&cnt=10&mode=json&appid=89575d3c850c4fe09a01e9aedf6aec9e"
--- RainyShinyCloudy/WeatherVC.swift
+++ RainyShinyCloudy/WeatherVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
@@ -19,6 +20,8 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     var currentWeather: CurrentWeather!
+    var forecast: Forecast!
+    var forecasts = [Forecast]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,8 +30,32 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         
         currentWeather = CurrentWeather()
+        
         currentWeather.downloadWeatherDetails {
-            self.updateMainUI()
+            self.downloadForecastData {
+                self.updateMainUI()
+            }
+        }
+    }
+    
+    func downloadForecastData(completed: @escaping DownloadComplete) {
+        //Downloading forecast weather data for TableView
+        let forecastURL = URL(string: FORECAST_URL)!
+        Alamofire.request(forecastURL).responseJSON { response in
+            let result = response.result
+            
+            if let dict = result.value as? Dictionary<String, Any> {
+                if let list = dict["list"] as? [Dictionary<String, Any>] {
+                    
+                    for obj in list {
+                        let forecast = Forecast(weatherDict: obj)
+                        self.forecasts.append(forecast)
+                        print(obj)
+                    }
+                    
+                }
+            }
+            completed()
         }
     }
 
